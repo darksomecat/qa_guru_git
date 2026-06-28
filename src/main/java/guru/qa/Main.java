ca77a54e05e1a83b80131a0bdc7afd471ced0068
Anna Ukolova <anikadd3@gmail.com>
2026-06-28 22:11:15 +0300

    new conflict create

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -6,7 +6,8 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello qa_guru new version pull in brunch1!");
+        System.out.print("Hello qa_guru new version pull in brunch for conflict!");
+        System.out.print("get me conflict!");
 
     }
 }
